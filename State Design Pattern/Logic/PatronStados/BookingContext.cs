f4174c1a802ac1d34ed9d75a6acecc8bd7027a2b
David Fortea <fortea84@gmail.com>
2021-01-31 12:48:57 +0100

    Se implementa BookingContext. Cambio carpetas.

--- State Design Pattern/Logic/PatronStados/BookingContext.cs
+++ State Design Pattern/Logic/PatronStados/BookingContext.cs
@@ -13,25 +13,34 @@ namespace State_Design_Pattern.Logic
         public string Attendee { get; set; }
         public int TicketCount { get; set; }
         public int BookingID { get; set; }
+        private BookingState currentState;
+
+        public void TransitionToState(BookingState state)
+        {
+            currentState = state;
+            currentState.EnterState(this);
+        }
+
 
         public BookingContext(MainWindow view)
         {
             View = view;
+            TransitionToState(new NewState());
         }
 
         public void SubmitDetails(string attendee, int ticketCount)
         {
-            
+            currentState.EnterDetails(this, attendee, ticketCount);
         }
 
         public void Cancel()
         {
-            
+            currentState.Cancel(this);
         }
 
         public void DatePassed()
         {
-           
+            currentState.DatePassed(this);
         }
 
         public void ShowState(string stateName)
